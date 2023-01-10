9bf7907dd914ac61a31848846a66fb95a4724424
Mahaldi Wijaya Saputra <aldiwijayasaputra@gmail.com>
2023-01-10 16:34:49 +0700

    add border radius props to image component

--- src/components/Image/index.jsx
+++ src/components/Image/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { ImageLoader } from './style'
 export const Image = props => {
-	const { src, alt, width, height} = props
+	const { src, alt, width, height, borderradius} = props
 	return (
 		<>
-			<ImageLoader alt={alt} src={src} widht={width} height={height} />
+			<ImageLoader alt={alt} src={src} width={width} height={height} borderradius={borderradius}/>
 		</>
 	)
+}
+Image.defaultProps = {
+	borderradius: '10px 10px 10px 10px',
 }
--- src/components/Image/style.js
+++ src/components/Image/style.js
@@ -2,5 +2,5 @@ import styled from 'styled-components'
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 
 export const ImageLoader = styled(LazyLoadImage)`
-	border-radius: 10px;
+	border-radius: ${({ borderradius }) => borderradius};
 `
