15bfdc8ae53cfaa844a40666e148c5c5bddf42eb
Mahaldi Wijaya Saputra <aldiwijayasaputra@gmail.com>
2023-01-12 16:26:23 +0700

    layout style

---
+++ src/layouts/style.js
@@ -0,0 +1,13 @@
+import { default as styledComponent } from 'styled-components'
+
+export const FooterWrapper = styledComponent.div`
+	height: auto;
+	display: flex;
+	justify-content: center;
+	position: relative;
+	width: 100%;
+`
+
+export const FooterContainer = styledComponent.div`
+	width: 82.5%;
+`
