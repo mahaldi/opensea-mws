9614881124e79097e577ee14fb42a3c4d468b824
Mahaldi Wijaya Saputra <aldiwijayasaputra@gmail.com>
2023-01-16 21:16:59 +0700

    change layout to children styling

--- src/layouts/style.js
+++ src/layouts/style.js
@@ -13,14 +13,10 @@ export const FooterWrapper = styledComponent.div`
 export const FooterContainer = styledComponent.div`
 	width: 82.5%;
 `
-export const ChildrenContainer = styledComponent.div`
-	margin-top: 130px;
-	padding: 0 64px;
-`
 
 export const Container = styled(Grid)`
 	> div:nth-of-type(2) {
-		margin-top: 150px;
-		padding: 0 64px;
+		padding-left: 64px;
+		padding-right: 64px;
 	}
 `
